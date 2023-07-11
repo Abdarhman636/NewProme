df933bf65ddfbb8c598eb8c3214a62a823bf6d21
Abdarhman <abdarhmanalsyaed@yahoo.com>
2023-07-11 20:35:13 +0300

    Second test

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,3 +1,37 @@
+let sun = document.getElementById('sun')
+let sahab = document.getElementById('sahab')
+let prt3 = document.getElementById('prt3')
+let heroText = document.getElementById('hero-text')
+let prt4 = document.getElementById('prt4')
+let prt5 = document.getElementById('prt5')
+let prt6 = document.getElementById('prt6')
+let prt7 = document.getElementById('prt7')
+let prt8 = document.getElementById('prt8')
+let prt9 = document.getElementById('prt9')
+let prt10 = document.getElementById('prt10')
+let aboutUs = document.getElementsByClassName('about-content')
+
+// let rotate11 = document.getElementsByClassName('rotate1')
+
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    heroText.style.bottom = value * 0.8 + 'px'
+    prt6.style.bottom = value * 1 + 'px'
+        // prt10.style.bottom = value * 0.1 + 'px'
+    sun.style.bottom = value * 0.2 + 'px'
+    prt4.style.top = value * 0.1 + 'px'
+    prt5.style.top = value * 0.2 + 'px'
+    prt7.style.top = value * 0.2 + 'px'
+        //     prt4.style.top = value * 0.05 + 'px'
+        //     prt8.style.bottom = value * 0.1 + 'px'
+        //     prt3.style.top = value * 0.5 + 'px'
+        //     prt4.style.top = value * 0.1 + 'px'
+})
+
+
+
+// --------------------------------------------Mockups-----------------------------------
 gsap.registerPlugin(ScrollTrigger)
 
 gsap.to('#mock1', {
@@ -45,3 +79,18 @@ gsap.from('.about-content', {
         // markers: true,
     }
 })
+
+// --------------------------------------------statistics-----------------------------------
+// var ProjectCounter = document.getElementById('pCounter')
+// var observer = new IntersectionObserver(function(entries) {
+//     // isIntersecting is true when element and viewport are overlapping
+//     // isIntersecting is false when element and viewport don't overlap
+//     if (entries[0].isIntersecting === true) {
+//         console.log('Element has just become visible in screen');
+//         for (var i = 0; i < 5; i++) {
+//             console.log('test')
+//         }
+//     }
+// }, { threshold: [0] });
+
+// observer.observe(document.querySelector(".statistics"));
